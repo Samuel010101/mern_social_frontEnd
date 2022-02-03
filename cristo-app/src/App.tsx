ec04135d456cb1d46d6ba12eb3adc379eb9d71ad
Sam Minyety <samuelmartinez11591.sm@gmail.com>
2022-02-03 12:27:38 -0400

    firt commit, creating react project

---
+++ cristo-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Logo from './components/Logo';
+import Publication from './components/Publication';
+
+function App() {
+  return (
+    <div>
+      <Logo />
+      <Publication />
+    </div>
+  );
+}
+
+export default App;
---
+++ cristo-app/src/components/Publication.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Publication = () => {
+  return (
+    <div>
+      <h3>Publication</h3>
+    </div>
+  );
+};
+
+export default Publication;
